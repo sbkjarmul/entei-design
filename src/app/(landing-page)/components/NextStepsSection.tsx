4cca366c42652151c0b1b2bcec43b8a506cf8847
ENTEI <entei@MacBook-Pro-Entei.local>
2025-06-17 13:52:31 +0100

    create NextStepsSection

---
+++ src/app/(landing-page)/components/NextStepsSection.tsx
@@ -0,0 +1,25 @@
+import AccentText from "../../../components/AccentText";
+import Heading from "../../../components/Heading";
+import Text from "../../../components/Text";
+
+export default function NextStepsSection() {
+  return (
+    <section
+      className="bg-white py-20 flex flex-col items-center gap-10"
+      id="ourwork"
+    >
+      <div className="flex flex-col items-center gap-4">
+        <div className="flex flex-col gap-2 items-center">
+          <AccentText>Skup się na swoim biznesie!</AccentText>
+          <Heading level={2} className="lg:text-7xl text-black text-center">
+            Rozumiemy jak bardzo zajęty jesteś
+          </Heading>
+        </div>
+        <Text className="text-center">
+          Chetnie zajmiemy sie regularnym tworzeniem treści na Twoje social
+          media, a także pozyskiwaniem klientów poprzez digital marketing.
+        </Text>
+      </div>
+    </section>
+  );
+}
