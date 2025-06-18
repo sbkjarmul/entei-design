058b262338441dff8c12513e11ad4927979c2fdf
ENTEI <entei@MacBook-Pro-Entei.local>
2025-06-18 10:19:57 +0100

    edit mobile form styling

--- src/app/(landing-page)/contact/page.tsx
+++ src/app/(landing-page)/contact/page.tsx
@@ -5,16 +5,16 @@ import ContactInfo from "./components/ContactInfo";
 
 export default function Contact() {
   return (
-    <div className="inset-0 flex w-screen min-h-fit items-center justify-center p-4 md:p-12">
-      <div className="flex flex-col lg:flex-row gap-12 p-2 rounded-lg w-full max-w-[1200px] max-h-[900px] min-h-fit">
-        <div className="w-full lg:w-1/2 flex z-1 justify-center">
+    <div className="inset-0 flex w-screen min-h-fit items-center justify-center p-0 md:p-8">
+      <div className="flex flex-col lg:flex-row gap-12 rounded-lg w-full max-w-[1200px] max-h-[900px] min-h-fit">
+        <div className="w-full lg:w-1/2 flex z-1 justify-center p-4">
           <ContactInfo />
         </div>
         <div className="p-0 w-full lg:w-1/2 h-full flex flex-col z-1">
           <ContactBox />
         </div>
 
-        <div className="absolute inset-0 bottom-[-50%] translate-y-[25%] lg:translate-y-[10%] lg:inset-[-15%] flex items-center justify-end z-0 overflow-hidden select-none ">
+        <div className="absolute inset-0 bottom-[-50%] translate-y-[25%] lg:translate-y-[10%] lg:inset-[-15%] flex items-center justify-end z-0 overflow-hidden select-none">
           <Image
             src="/images/entei-logo-blurred.png"
             alt="Entei Design Background"
--- src/components/Header/HeaderButton.tsx
+++ src/components/Header/HeaderButton.tsx
@@ -9,11 +9,11 @@ export default function HeaderButton() {
 
   return (
     <button
-      className="h-full px-10 bg-primary text-black text-2xl font-normal hover:bg-primary/90 transition-colors cursor-pointer
+      className="h-full px-4 md:px-10 bg-primary text-black text-regular md:text-2xl font-normal hover:bg-primary/90 transition-colors cursor-pointer
       focus-visible:outline-none focus-visible:ring-4 focus-visible:ring-black focus-visible:ring-offset-2 focus-visible:ring-offset-primary"
       onClick={handleButtonClick}
     >
-      <div className="flex flex-col items-center leading-7">
+      <div className="flex flex-col items-center leading-tight md:leading-7">
         <span>Porozmawiajmy o</span>
         <span>
           <span className="font-bold">Twoim</span> projekcie
