cd22186146fa8de21734b0c3ef5b210cd7730d5d
ENTEI <entei@MacBook-Pro-Entei.local>
2025-07-10 16:13:30 +0200

    update our clients logos

--- src/app/(landing-page)/components/OurClientsSection.tsx
+++ src/app/(landing-page)/components/OurClientsSection.tsx
@@ -16,51 +16,63 @@ export default function OurClientsSection() {
           Pomogliśmy już 20+ firmom wyglądać lepiej
         </Text>
       </div>
-      <div className="flex flex-col items-center gap-y-10 max-w-5xl w-full">
-        <div className="flex flex-row justify-center gap-x-16 w-full">
-          <Link href="https://art-mat.com.pl" target="_blank">
-            <Image
-              src="/images/our-clients/art-mat-logo.png"
-              alt="art-mat"
-              width={200}
-              height={40}
-              sizes="(max-width: 768px) 100px, 200px"
-              className="object-contain w-auto h-full grayscale opacity-50 hover:opacity-100 transition-opacity duration-300"
-            />
-          </Link>
-          <Link href="https://rem-met.com" target="_blank">
-            <Image
-              src="/images/our-clients/rem-met-logo.png"
-              alt="rem-met"
-              width={200}
-              height={40}
-              sizes="(max-width: 768px) 100px, 200px"
-              className="object-contain w-auto h-full grayscale opacity-50 hover:opacity-100 transition-opacity duration-300"
-            />
-          </Link>
-        </div>
-        <div className="flex flex-row justify-center gap-x-16 w-full">
-          <Link href="https://lagomwarsaw.com" target="_blank">
-            <Image
-              src="/images/our-clients/lagom-logo.png"
-              alt="lagom"
-              width={200}
-              height={40}
-              sizes="(max-width: 768px) 100px, 200px"
-              className="object-contain w-auto h-full grayscale opacity-50 hover:opacity-100 transition-opacity duration-300"
-            />
-          </Link>
-          <Link href="https://aristo-pharma.pl" target="_blank">
-            <Image
-              src="/images/our-clients/aristo-logo.png"
-              alt="aristo"
-              width={200}
-              height={40}
-              sizes="(max-width: 768px) 100px, 200px"
-              className="object-contain w-auto h-full grayscale opacity-50 hover:opacity-100 transition-opacity duration-300"
-            />
-          </Link>
-        </div>
+      <div className="grid grid-cols-2 gap-8 md:gap-16 max-w-5xl w-full">
+        <Link
+          href="https://art-mat.com.pl"
+          target="_blank"
+          className="flex justify-end items-center"
+        >
+          <Image
+            src="/images/our-clients/art-mat-logo.png"
+            alt="art-mat"
+            width={400}
+            height={100}
+            sizes="(max-width: 768px) 300px, 500px"
+            className="object-contain w-auto h-full grayscale opacity-50 hover:opacity-100 transition-opacity duration-300"
+          />
+        </Link>
+        <Link
+          href="https://rem-met.com"
+          target="_blank"
+          className="flex justify-start items-center"
+        >
+          <Image
+            src="/images/our-clients/rem-met-logo.png"
+            alt="rem-met"
+            width={400}
+            height={100}
+            sizes="(max-width: 768px) 300px, 500px"
+            className="object-contain w-auto h-full grayscale opacity-50 hover:opacity-100 transition-opacity duration-300"
+          />
+        </Link>
+        <Link
+          href="https://lagomwarsaw.com"
+          target="_blank"
+          className="flex justify-end items-center"
+        >
+          <Image
+            src="/images/our-clients/lagom-logo.png"
+            alt="lagom"
+            width={400}
+            height={100}
+            sizes="(max-width: 768px) 200px, 300px"
+            className="object-contain w-auto h-full grayscale opacity-50 hover:opacity-100 transition-opacity duration-300"
+          />
+        </Link>
+        <Link
+          href="https://aristo-pharma.pl"
+          target="_blank"
+          className="flex justify-start items-center"
+        >
+          <Image
+            src="/images/our-clients/aristo-logo.png"
+            alt="aristo"
+            width={400}
+            height={100}
+            sizes="(max-width: 768px) 200px, 400px"
+            className="object-contain w-auto h-full grayscale opacity-50 hover:opacity-100 transition-opacity duration-300"
+          />
+        </Link>
       </div>
     </Section>
   );
