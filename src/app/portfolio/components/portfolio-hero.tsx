96d3829c62d29fc9f8a03310a4111f16fd28a546
ENTEI <entei@MacBook-Pro-Entei.local>
2025-08-01 12:53:15 +0200

    update mobile view

--- src/app/portfolio/components/portfolio-hero.tsx
+++ src/app/portfolio/components/portfolio-hero.tsx
@@ -10,11 +10,52 @@ function PortfolioHeroShadow() {
   );
 }
 
-export default function PortfolioHero() {
+function PortfolioHeroText() {
   const t = useTranslations("portfolio");
 
   return (
-    <section className="w-full h-screen bg-[#0C0C0C] relative max-h-[1000px]">
+    <div className="flex flex-col items-start justify-end gap-4">
+      <div className="flex items-center gap-2 py-2 px-4 border border-gray-800 rounded-full bg-gray-900">
+        <div className="w-4 h-4 bg-primary rounded-full animate-pulse shadow-[0_0_10px_var(--color-primary)]" />
+        <span className="text-white">{t("hero.available")}</span>
+      </div>
+
+      <Heading level={1} className="text-white">
+        {t("hero.title")}
+      </Heading>
+    </div>
+  );
+}
+
+function PortfolioHeroFigmaComment() {
+  const t = useTranslations("portfolio");
+
+  return (
+    <div className="p-3 border border-gray-800 rounded-tl-4xl rounded-tr-4xl rounded-br-4xl rounded-bl-0  bg-gray-900 flex items-start justify-start gap-2 w-[320px] shadow-2xl">
+      <div className="p-2 min-w-10 min-h-10 bg-black rounded-full flex items-center justify-center">
+        <Image
+          src="/images/portfolio/entei-logo.svg"
+          alt="ENTEI Logo in avatar"
+          width={20}
+          height={20}
+          className="object-contain"
+          priority
+        />
+      </div>
+      <div className="flex flex-col gap-0">
+        <div className="flex items-center gap-2">
+          <span className="text-white font-medium">{t("hero.entei")}</span>
+          <span className="text-gray-600 font-light">{t("hero.now")}</span>
+        </div>
+        <span className="text-xl font-light">{t("hero.comment")}</span>
+      </div>
+    </div>
+  );
+}
+
+export default function PortfolioHero() {
+  return (
+    <section className="w-full h-screen bg-[#0C0C0C] relative max-h-[600px] md:max-h-[1000px]">
       <PortfolioHeroShadow />
 
       <Image
@@ -26,41 +67,18 @@ export default function PortfolioHero() {
         priority
       />
 
-      <Container className="relative h-full z-2">
-        <div className="w-full h-full flex flex-col items-start justify-end">
-          <div className="absolute top-1/3 left-0 w-[450px] flex flex-col items-start justify-end gap-4">
-            <div className="flex items-center gap-2 py-2 px-4 border border-gray-800 rounded-full  bg-gray-900">
-              <div className="w-4 h-4 bg-primary rounded-full animate-pulse shadow-[0_0_10px_var(--color-primary)]" />
-              <span className="text-white">{t("hero.available")}</span>
-            </div>
-
-            <Heading level={1} className="text-white">
-              {t("hero.title")}
-            </Heading>
+      <Container className="relative h-full z-2 ">
+        <div className="w-full h-full flex flex-col items-start justify-end gap-10">
+          <div className="hidden md:block absolute top-1/3 left-0 w-[450px]">
+            <PortfolioHeroText />
           </div>
 
-          <div className="absolute top-1/3 left-2/3 p-3 border border-gray-800 rounded-tl-4xl rounded-tr-4xl rounded-br-4xl rounded-bl-0  bg-gray-900 flex items-start justify-start gap-2 w-[320px] shadow-2xl">
-            <div className="p-2 min-w-10 min-h-10 bg-black rounded-full flex items-center justify-center">
-              <Image
-                src="/images/portfolio/entei-logo.svg"
-                alt="ENTEI Logo in avatar"
-                width={20}
-                height={20}
-                className="object-contain"
-                priority
-              />
-            </div>
-            <div className="flex flex-col gap-0">
-              <div className="flex items-center gap-2">
-                <span className="text-white font-medium">
-                  {t("hero.entei")}
-                </span>
-                <span className="text-gray-600 font-light">
-                  {t("hero.now")}
-                </span>
-              </div>
-              <span className="text-xl font-light">{t("hero.comment")}</span>
-            </div>
+          <div className="hidden md:block absolute top-1/3 left-2/3 ">
+            <PortfolioHeroFigmaComment />
+          </div>
+
+          <div className="block md:hidden px-4">
+            <PortfolioHeroText />
           </div>
 
           <PortfolioHeroShadow />
