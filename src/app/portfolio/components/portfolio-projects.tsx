70552d1c08728a29f0fa42c8d67ec0aaf0336a04
ENTEI <entei@MacBook-Pro-Entei.local>
2025-08-06 17:51:14 +0200

    add motion

--- src/app/portfolio/components/portfolio-projects.tsx
+++ src/app/portfolio/components/portfolio-projects.tsx
@@ -3,18 +3,20 @@
 import Container from "@/components/Container";
 import Heading from "@/components/Heading";
 import { motion } from "framer-motion";
+import { useTranslations } from "next-intl";
 import Image from "next/image";
 import Link from "next/link";
 
 export default function PortfolioProjects() {
+  const t = useTranslations("portfolio");
   return (
     <section className="h-screen bg-gray-950 relative" id="projects">
       <Container className="h-screen flex flex-col items-center justify-center bg-gray-950 relative gap-20 px-4  py-10 md:py-20">
         <div className="flex flex-col items-center justify-center gap-4">
           <Heading level={2}>
             <span className="relative flex flex-col gap-0 py-2 text-5xl md:text-7xl">
-              <span className="text-gray-500">So...</span>
-              <span className="text-white">How do I work?</span>
+              <span className="text-gray-500">{t("projects.so")}</span>
+              <span className="text-white">{t("projects.howDoIWork")}</span>
 
               <motion.div
                 initial={{ width: "100%" }}
@@ -38,8 +40,7 @@ export default function PortfolioProjects() {
             whileInView={{ opacity: 1 }}
             transition={{ duration: 1, delay: 1.5 }}
           >
-            Explore the results of my work through detailed case studies or
-            live, fully launched websites.
+            {t("projects.description")}
           </motion.p>
         </div>
         <motion.div
@@ -51,6 +52,7 @@ export default function PortfolioProjects() {
           <Link
             href="/case-study/suseu"
             className="opacity-50 hover:opacity-100 transition-opacity duration-300"
+            target="_blank"
           >
             <Image
               src="/images/case-studies/suseu-case-study-logo.svg"
@@ -63,6 +65,7 @@ export default function PortfolioProjects() {
           <Link
             href="https://www.rem-met.com"
             className="opacity-50 hover:opacity-100 transition-opacity duration-300"
+            target="_blank"
           >
             <Image
               src="/images/case-studies/rem-met-case-study-logo.svg"
