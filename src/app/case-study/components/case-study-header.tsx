057729140e8cd049d0d5f7c0b63ef0d0fe064261
ENTEI <entei@MacBook-Pro-Entei.local>
2025-08-13 17:59:34 +0200

    update mobile version

--- src/app/case-study/components/case-study-header.tsx
+++ src/app/case-study/components/case-study-header.tsx
@@ -6,9 +6,9 @@ export default function PortfolioHeader() {
   return (
     <header className="fixed top-0 z-50 w-full bg-background/95">
       <Container className="h-[100px] flex items-center justify-between px-8 max-w-none">
-        <Link href="/" className="relative h-10 w-10">
+        <Link href="/portfolio" className="relative h-10 w-10">
           <Image
-            src="/images/entei-logo.svg"
+            src="/images/entei-logo-gray.svg"
             alt="Entei Design"
             fill
             className="object-contain"
