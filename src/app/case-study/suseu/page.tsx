018003ee22dcb4ae8a0debce9a797b7b8d120aaa
ENTEI <entei@MacBook-Pro-Entei.local>
2025-08-13 18:52:22 +0200

    set padding to lg

--- src/app/case-study/suseu/page.tsx
+++ src/app/case-study/suseu/page.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 
 export default function PortfolioSuseuCaseStudy() {
   return (
-    <section className="min-h-screen bg-gray-800 relative md:py-16">
-      <Container className="md:shadow-[0_0_24px_rgba(0,0,0,0.9)] md:border-l md:border-r md:border-t border-gray-700">
+    <section className="min-h-screen bg-gray-800 relative lg:py-16">
+      <Container className="md:shadow-[0_0_24px_rgba(0,0,0,0.9)] md:border border-gray-700">
         <div className="relative w-full h-auto ">
           <Image
             src="/images/case-studies/suseu/suseu-overview.png"
