50a741e930cb864045b12df19b97bdebe466621c
ENTEI <entei@MacBook-Pro-Entei.local>
2025-08-13 18:04:25 +0200

    fix header portfolio

--- src/app/portfolio/components/portfolio-header.tsx
+++ src/app/portfolio/components/portfolio-header.tsx
@@ -6,10 +6,10 @@ export default function PortfolioHeader() {
   return (
     <header className="fixed top-0 z-50 w-full bg-background/95">
       <Container className="h-[100px] flex items-center justify-between px-4 md:pr-0 xl:pl-0">
-        <Link href="/" className="relative h-10 w-10">
+        <Link href="/portfolio" className="relative h-10 w-10">
           <Image
             src="/images/entei-logo.svg"
-            alt="Entei Design"
+            alt="Logo ENTEI"
             fill
             className="object-contain"
             priority
