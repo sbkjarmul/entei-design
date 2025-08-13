1cc42feb387103e64f5147f0eefdf05f98fa0868
ENTEI <entei@MacBook-Pro-Entei.local>
2025-08-13 17:52:50 +0200

    add susue case study 1/8

--- src/app/case-study/components/case-study-header.tsx
+++ src/app/case-study/components/case-study-header.tsx
@@ -5,7 +5,7 @@ import Container from "@/components/Container";
 export default function PortfolioHeader() {
   return (
     <header className="fixed top-0 z-50 w-full bg-background/95">
-      <Container className="h-[100px] flex items-center justify-between px-4 md:pr-0 xl:pl-0">
+      <Container className="h-[100px] flex items-center justify-between px-8 max-w-none">
         <Link href="/" className="relative h-10 w-10">
           <Image
             src="/images/entei-logo.svg"
