a88b6e93936adf22ff2fa0c154df5a916c8c6593
ENTEI <entei@MacBook-Pro-Entei.local>
2025-08-13 17:52:50 +0200

    add susue case study 1/8

--- src/app/case-study/layout.tsx
+++ src/app/case-study/layout.tsx
@@ -1,10 +1,13 @@
-export default function PortfolioTemplate({
+import CaseStudyHeader from "./components/case-study-header";
+
+export default function CaseStudyLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
     <div className="min-h-screen flex flex-col">
+      <CaseStudyHeader />
       <main className="flex-grow">{children}</main>
     </div>
   );
--- src/app/case-study/suseu/page.tsx
+++ src/app/case-study/suseu/page.tsx
@@ -1,12 +1,46 @@
+import Container from "@/components/Container";
+import Image from "next/image";
+
 export default function PortfolioSuseuCaseStudy() {
   return (
-    <section className="h-screen bg-gray-950 relative">
-      <iframe
-        src="/images/case-studies/suseu-case-study-eng.pdf"
-        width="100%"
-        height="1000px"
-        style={{ border: "none" }}
-      />
+    <section className="min-h-screen bg-gray-800 relative py-16">
+      <Container className="shadow-[0_0_24px_rgba(0,0,0,0.9)] border-l border-r border-t border-gray-700">
+        <div className="relative w-full h-auto ">
+          <Image
+            src="/images/case-studies/suseu/suseu-overview.png"
+            alt="Suseu Case Study"
+            width={0}
+            height={0}
+            sizes="100vw"
+            className="w-full h-auto"
+            priority
+          />
+        </div>
+
+        <div className="relative w-full h-auto">
+          <Image
+            src="/images/case-studies/suseu/suseu-design-process.png"
+            alt="Suseu Case Study"
+            width={0}
+            height={0}
+            sizes="100vw"
+            className="w-full h-auto"
+            priority
+          />
+        </div>
+
+        <div className="relative w-full h-auto">
+          <Image
+            src="/images/case-studies/suseu/suseu-market-research.png"
+            alt="Suseu Case Study"
+            width={0}
+            height={0}
+            sizes="100vw"
+            className="w-full h-auto"
+            priority
+          />
+        </div>
+      </Container>
     </section>
   );
 }
