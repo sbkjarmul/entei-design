cec3f746d2c99da634ab92c3af262096849a1522
ENTEI <entei@MacBook-Pro-Entei.local>
2025-08-13 17:59:34 +0200

    update mobile version

--- src/app/case-study/suseu/page.tsx
+++ src/app/case-study/suseu/page.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 
 export default function PortfolioSuseuCaseStudy() {
   return (
-    <section className="min-h-screen bg-gray-800 relative py-16">
-      <Container className="shadow-[0_0_24px_rgba(0,0,0,0.9)] border-l border-r border-t border-gray-700">
+    <section className="min-h-screen bg-gray-800 relative md:py-16">
+      <Container className="md:shadow-[0_0_24px_rgba(0,0,0,0.9)] md:border-l md:border-r md:border-t border-gray-700">
         <div className="relative w-full h-auto ">
           <Image
             src="/images/case-studies/suseu/suseu-overview.png"
@@ -25,7 +25,6 @@ export default function PortfolioSuseuCaseStudy() {
             height={0}
             sizes="100vw"
             className="w-full h-auto"
-            priority
           />
         </div>
 
@@ -37,7 +36,17 @@ export default function PortfolioSuseuCaseStudy() {
             height={0}
             sizes="100vw"
             className="w-full h-auto"
-            priority
+          />
+        </div>
+
+        <div className="relative w-full h-auto">
+          <Image
+            src="/images/case-studies/suseu/suseu-mobile-user-persona.png"
+            alt="Suseu Case Study"
+            width={0}
+            height={0}
+            sizes="100vw"
+            className="w-full h-auto"
           />
         </div>
       </Container>
