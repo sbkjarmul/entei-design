8985be5f24851a79348c0af8e69ef2146b9b9812
ENTEI <entei@MacBook-Pro-Entei.local>
2025-09-19 15:51:11 +0200

    update hero link

--- src/app/(landing-page)/components/HeroSection.tsx
+++ src/app/(landing-page)/components/HeroSection.tsx
@@ -1,17 +1,15 @@
 "use client";
 
+import Link from "next/link";
+import Image from "next/image";
+
 import Button from "../../../components/Button";
 import AccentText from "../../../components/AccentText";
 import Heading from "../../../components/Heading";
 import Text from "../../../components/Text";
-import { useRouter } from "next/navigation";
 import Section from "@/components/Section";
 
 export default function HeroSection() {
-  const router = useRouter();
-
-  const handleBookMeeting = () => router.push("/contact");
-
   return (
     <Section id="hero" className="items-center justify-center text-center px-6">
       <div className="flex flex-col gap-2 items-center">
@@ -36,15 +34,19 @@ export default function HeroSection() {
         </span>
       </Text>
       <div className="flex flex-col sm:flex-row gap-4 mt-4 w-full justify-center">
-        <Button
-          variant="primary"
-          onClick={handleBookMeeting}
-          isHero
-          className="relative z-1"
-        >
-          <div className="absolute inset-[-4px] bg-primary/50 rounded-xl animate-pulse z-0 blur-sm" />
-          <span className="z-1">Umów konsultacje</span>
-        </Button>
+        <Link href={"https://calendly.com/entei-designs/30min"} target="_blank">
+          <Button className="relative z-1">
+            <div className="absolute inset-[-4px] bg-primary/50 rounded-xl animate-pulse z-0 blur-sm" />{" "}
+            <span className="z-1">Umów konsultację</span>
+            <Image
+              src="/images/icons/arrow-icon-black.svg"
+              alt="Arrow Icon"
+              width="20"
+              height="20"
+              className="z-2"
+            />
+          </Button>
+        </Link>
       </div>
     </Section>
   );
